16cf061e883773b9568a6a79b6488875b17e1a62
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-14 15:02:44 -0300

    :sparkles: Add entry for production static application.

---
+++ index.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const PORT = +process.env.PORT || 3000
+
+app.use(express.static(path.resolve(__dirname, 'app')))
+
+app.listen(PORT, () => {
+  console.log(`Server started at "http://0.0.0.0:${PORT}/".`)
+})
