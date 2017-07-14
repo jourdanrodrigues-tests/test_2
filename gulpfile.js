829cd77fb6c47c060dee21b9b75ef7e0a2316678
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-14 17:22:41 -0300

    :ambulance: :snowflake: Use Gulp for SASS (according to specifications).

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+
+const sassFiles = path.resolve(__dirname, 'app', '**', '*.scss')
+
+gulp.task('compile:css', () =>
+  gulp.src(sassFiles)
+    .pipe(sass())
+    .pipe(gulp.dest('app'))
+)
+
+gulp.task('compile:css:watch', ['compile:css'], () => {
+  gulp.watch(sassFiles, ['compile:css'])
+})
