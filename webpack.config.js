85a6a223935793c198fb277ddb29c3969c6c9573
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-14 15:01:48 -0300

    :art: Expand WebPack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path = require('path')
+
 module.exports = {
-  entry: './index.js',
+  entry: path.resolve(__dirname, '/app/js/index.js'),
   output: {
-    path: './src',
+    path: path.resolve(__dirname, '/app/'),
     filename: 'app.bundle.js'
   },
   module: {
@@ -9,9 +11,9 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          {loader: "style-loader"}, // creates style nodes from JS strings
-          {loader: "css-loader"}, // translates CSS into CommonJS
-          {loader: "sass-loader"} // compiles Sass to CSS
+          {loader: 'style-loader'}, // creates style nodes from JS strings
+          {loader: 'css-loader'}, // translates CSS into CommonJS
+          {loader: 'sass-loader'} // compiles Sass to CSS
         ]
       },
       {
@@ -21,4 +23,4 @@ module.exports = {
       }
     ]
   }
-};
+}
