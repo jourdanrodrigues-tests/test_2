be3aa45e3c871a39e9602a026a553885b64f4841
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-15 14:37:27 -0300

    :sparkles: Add link to future profile page.

--- src/login/index.js
+++ src/login/index.js
@@ -1,3 +1,13 @@
 import './index.css'
 import 'material-design-lite/dist/material.min.css'
+
 import 'material-design-lite'
+import '../js/docReady'
+
+docReady(() => {
+  document.querySelector('.form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    debugger
+    window.location.href = '/profile/'
+  })
+})
