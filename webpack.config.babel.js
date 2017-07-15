669768031694a3daef3bc9354196f62a4452356f
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-14 21:44:43 -0300

    :ambulance: :snowflake: Still removing traces of SASS on where it does not belong.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -17,14 +17,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: [
-          {loader: 'style-loader'}, // creates style nodes from JS strings
-          {loader: 'css-loader'}, // translates CSS into CommonJS
-          {loader: 'sass-loader'} // compiles Sass to CSS
-        ]
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
