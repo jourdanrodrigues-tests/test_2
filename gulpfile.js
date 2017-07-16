e9b72db25d37f291d95a5a232f1ffe4a43ba39e1
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-16 16:13:51 -0300

    :snowflake: CSS cross-browser compatibility.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
 
 const sassFiles = path.resolve(__dirname, 'src', '**', '*.scss')
 
 gulp.task('compile:css', () =>
   gulp.src(sassFiles)
     .pipe(sass())
+    .pipe(autoprefixer({browsers: ['last 50 versions'], cascade: false}))
     .pipe(gulp.dest('src'))
 )
 
