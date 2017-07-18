15df25b2056ee774607d7b699cf1b6af9e2de236
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-18 18:04:30 -0300

    :tractor: Slip profile JS files.

--- src/profile/index.js
+++ src/profile/index.js
@@ -3,27 +3,10 @@ import 'ionicons/dist/css/ionicons.min.css'
 import './index.css'
 
 import 'material-design-lite'
-import '../js/docReady'
+
+import './js/tabs'
+import './js/firefox'
 
 window.logout = () => {
   window.location.href = '/login/'
 }
-
-docReady(() => {
-  // Firefox handler: Some things might be need just for it
-  if (navigator.userAgent.indexOf('Firefox') > 0) {
-    document.querySelectorAll('[data-firefox-class]').forEach((item) => {
-      item.className += ` ${item.getAttribute('data-firefox-class')}--firefox`
-    })
-  }
-  // Tab handler
-  document.querySelectorAll('.profile__menu__tabs__item').forEach((item) => {
-    item.addEventListener('click', (event) => {
-      const activeClass = 'profile__menu__tabs__item--active'
-      if (event.target.classList.contains(activeClass)) { return }
-      let activeEl = document.querySelector(`.${activeClass}`)
-      activeEl.className = activeEl.className.replace(activeClass, '')
-      event.target.className += ` ${activeClass}`
-    })
-  })
-})
