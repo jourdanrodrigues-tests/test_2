584e0f5fb8f619d447e9347176aaa8ac7fa9d02e
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-18 19:50:55 -0300

    :sparkles: Popover completely functional.

--- src/profile/js/popover.js
+++ src/profile/js/popover.js
@@ -1,13 +1,10 @@
 import '../../js/docReady'
 
 docReady(() => {
-  document.querySelector('.editor__popover__button[data-for=cancel]').addEventListener('click', () => {
-    document.querySelector('.editor__popover').style.display = 'none'
-  })
   document.querySelectorAll('.editor__field__edit_box').forEach((item) => {
     item.addEventListener('click', (event) => {
       let content = event.target.parentElement.querySelector('.editor__field__content')
-        .innerHTML.replace(/(<i.+i>|^\s+|\s+$)/g, '')
+      let contentValue = content.innerHTML.replace(/(<i.+i>|^\s+|\s+$)/g, '')
 
       let popover = document.querySelector('.editor__popover')
       // As @sergio_caelum correctly pointed out, none of this will be "draw" on the screen till the end of the code
@@ -19,7 +16,30 @@ docReady(() => {
 
       let input = popover.querySelector('.editor__popover__field__input')
       input.focus()
-      input.setAttribute('value', content)
+      input.value = contentValue
+
+      let saveButton = popover.querySelector('.editor__popover__button[data-for=save]')
+      let cancelButton = popover.querySelector('.editor__popover__button[data-for=cancel]')
+      clearButtonListeners() // For the case the user clicks in other field without closing
+      saveButton.addEventListener('click', saveFunction)
+      cancelButton.addEventListener('click', closePopover)
+
+      function closePopover () {
+        clearButtonListeners()
+        input.value = ''
+        popover.style.display = 'none'
+      }
+
+      function clearButtonListeners () {
+        let empty = () => {}
+        saveButton.removeEventListener('click', empty)
+        cancelButton.removeEventListener('click', empty)
+      }
+
+      function saveFunction () {
+        content.innerHTML = content.innerHTML.replace(contentValue, input.value)
+        closePopover()
+      }
     })
   })
 })
