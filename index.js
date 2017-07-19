7d2cc9ab2837ec5329290d6b07d2db9590dda103
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-19 13:07:58 -0300

    :snowflake: Redirect root to login page.

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ const PORT = +process.env.PORT || 3000
 
 app.use(express.static(path.resolve(__dirname, 'src')))
 
+app.get('/', (req, res) => { res.redirect('/login/') })
+
 app.listen(PORT, () => {
   console.log(`Server started at "http://0.0.0.0:${PORT}/".`)
 })
