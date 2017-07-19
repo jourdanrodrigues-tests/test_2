921ab93fff3b9515663a0cc9490ef06f5692f97b
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-19 11:48:21 -0300

    :ambulanc: Detect IE as Firefox.

--- src/profile/js/browserHandler.js
+++ src/profile/js/browserHandler.js
@@ -5,12 +5,16 @@ if (!NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach
 }
 
+const firefox = navigator.userAgent.indexOf('Firefox') > 0
+const ie = navigator.appName === 'Microsoft Internet Explorer' || navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)
+const safari = navigator.userAgent.indexOf('Safari') > 0
+
 docReady(() => {
-  if (navigator.userAgent.indexOf('Firefox') > 0) {
+  if (firefox || ie) { // Same rules apply
     document.querySelectorAll('[data-browser-class]').forEach((item) => {
       item.className += ` ${item.getAttribute('data-browser-class')}--firefox`
     })
-  } else if (navigator.userAgent.indexOf('Safari') > 0) {
+  } else if (safari) {
     document.querySelectorAll('[data-browser-class]').forEach((item) => {
       item.className += ` ${item.getAttribute('data-browser-class')}--safari`
     })
